651efc3fb4f484ec63f38265157de5783a5f793e
Sachin Pokharel <sachinwork33@gmail.com>
2022-04-15 15:04:29 +0200

    updated readme

--- app.py
+++ app.py
@@ -205,5 +205,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=(os.environ.get("PORT", 5000)))
-    # app.run(debug=True)
+    #app.run('0.0.0.0', port=(os.environ.get("PORT", 5000)))
+     app.run(debug=True)
